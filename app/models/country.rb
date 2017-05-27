4755072fb8d43c2e3804e59d1cdd955eed97992c
Leo Salat <leo.salat@gmail.com>
2017-05-26 19:34:37 -0700

    add country model

---
+++ app/models/country.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: countries
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Country < ApplicationRecord
+end
