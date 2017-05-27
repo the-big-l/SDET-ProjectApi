7c9073223b63c19c8299e6de6620190b52c5fd77
Leo Salat <leo.salat@gmail.com>
2017-05-26 19:33:03 -0700

    remove project_id column and unique constraint on cost

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,6 @@
 # Table name: projects
 #
 #  id            :integer          not null, primary key
-#  project_id    :integer
 #  project_name  :string
 #  creation_date :datetime
 #  expiry_date   :datetime
@@ -15,16 +14,16 @@
 #
 
 class Project < ApplicationRecord
-  validates :project_id,
+  validates :id,
     :project_name,
     :enabled,
     :project_cost,
     :project_url,
     presence: true
 
-  validates :project_id, :project_name, :project_url, uniqueness: true
+  validates :id, :project_name, uniqueness: true
   validates :creation_date, :expiry_date, date: true
   validates :enabled, inclusion: { in: [true, false],
     message: "must be a boolean" }
-  validates :project_id, numericality: { only_integer: true }
+  validates :id, numericality: { only_integer: true }
 end
