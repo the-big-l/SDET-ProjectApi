ba0fff896e6c78495ca3954b639609ec62ce716d
Leo Salat <leo.salat@gmail.com>
2017-05-26 17:09:13 -0700

    add Project get and post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope defaults: { format: :json } do
+    post '/createProject', to: 'projects#create'
+    get '/requestProject', to: 'projects#show'
+  end
 end
