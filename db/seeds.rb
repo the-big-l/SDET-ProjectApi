b4c092c7d6b08f3f95925224f8dbbf46743ef696
Leo Salat <leo.salat@gmail.com>
2017-05-27 16:59:33 -0700

    clears database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+Country.destroy_all
+TargetCountry.destroy_all
