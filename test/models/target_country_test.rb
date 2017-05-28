95806adf0596314563ccf1a0c05f2c55bf22492c
Leo Salat <leo.salat@gmail.com>
2017-05-28 16:40:43 -0700

    annotate with db schema

--- test/models/target_country_test.rb
+++ test/models/target_country_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: target_countries
+#
+#  id         :integer          not null, primary key
+#  project_id :integer
+#  country_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class TargetCountryTest < ActiveSupport::TestCase
