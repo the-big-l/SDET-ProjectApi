897f4d3d599dcc6c8ba80f7ce81946acaa9f0f22
Leo Salat <leo.salat@gmail.com>
2017-05-30 16:24:49 -0700

    add logging; gitignore logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  logger           = ActiveSupport::Logger.new("development.log")
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  # config.log_level = :info
 end
